8e60b9c434a6e942b0feb0b5919fab5eb005bf28
tharun26 <tharun26@gmail.com>
2015-01-17 19:34:38 +0530

    First Commit

---
+++ app/src/main/java/com/inspiron/tharun26/manusys/HomeFragment.java
@@ -0,0 +1,26 @@
+package com.inspiron.tharun26.manusys;
+
+/**
+ * Created by tharun26 on 17/1/15.
+ */
+
+
+        import android.app.Fragment;
+        import android.os.Bundle;
+        import android.view.LayoutInflater;
+        import android.view.View;
+        import android.view.ViewGroup;
+
+public class HomeFragment extends Fragment {
+
+    public HomeFragment(){}
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+
+        return rootView;
+    }
+}
