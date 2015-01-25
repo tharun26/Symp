d5aa1ed7e66a4bb3712dd80f601af4708acb9220
tharun26 <tharun26@gmail.com>
2015-01-26 00:36:44 +0530

    PushNotification-Done

--- app/src/main/java/com/inspiron/tharun26/manusys/MyActivity.java
+++ app/src/main/java/com/inspiron/tharun26/manusys/MyActivity.java
@@ -3,8 +3,13 @@ package com.inspiron.tharun26.manusys;
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.res.Configuration;
 import android.content.res.TypedArray;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.widget.DrawerLayout;
@@ -14,15 +19,40 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import com.google.android.gcm.GCMRegistrar;
 import com.inspiron.tharun26.manusys.adapter.NavDrawerListAdapter;
 
 import java.util.ArrayList;
 
 import con.inspiron.tharun26.manusys.model.NavDrawerItem;
+import static com.inspiron.tharun26.manusys.CommonUtilities.DISPLAY_MESSAGE_ACTION;
+import static com.inspiron.tharun26.manusys.CommonUtilities.SENDER_ID;
+import static com.inspiron.tharun26.manusys.CommonUtilities.EXTRA_MESSAGE;
 
+
+/*import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+*/
 
 public class MyActivity extends Activity {
+
+/*
+GCM
+ */
+    ConnectionDetector cd;
+    public static String name;
+    public static String email;
+    AsyncTask<Void, Void, Void> mRegisterTask;
+
 
     private DrawerLayout mDrawerLayout;
     private ListView mDrawerList;
@@ -46,6 +76,106 @@ public class MyActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
 
+       /*GCM*/
+        cd = new ConnectionDetector(getApplicationContext());
+        // Check if Internet present
+        if (!cd.isConnectingToInternet()) {
+            // Internet Connection is not present
+            Toast.makeText(getApplicationContext(), "No Internet Connection", Toast.LENGTH_LONG).show();
+            // stop executing code by return
+            return;
+        }
+
+        name = "ManusysID";
+        email = "ManusysEmail";
+
+        // Make sure the device has the proper dependencies.
+        GCMRegistrar.checkDevice(this);
+
+        // Make sure the manifest was properly set - comment out this line
+        // while developing the app, then uncomment it when it's ready.
+        GCMRegistrar.checkManifest(this);
+
+        BroadcastReceiver mHandleMessageReceiver = null;
+        registerReceiver(mHandleMessageReceiver, new IntentFilter(
+                DISPLAY_MESSAGE_ACTION));
+
+        // Get GCM registration id
+        final String regId = GCMRegistrar.getRegistrationId(this);
+
+        // Check if regid already presents
+        if (regId.equals("")) {
+            // Registration is not present, register now with GCM
+            GCMRegistrar.register(this, SENDER_ID);
+        } else {
+            // Device is already registered on GCM
+            if (GCMRegistrar.isRegisteredOnServer(this)) {
+                // Skips registration.
+                Toast.makeText(getApplicationContext(), "Already registered with GCM", Toast.LENGTH_LONG).show();
+            } else {
+                // Try to register again, but not in the UI thread.
+                // It's also necessary to cancel the thread onDestroy(),
+                // hence the use of AsyncTask instead of a raw thread.
+                final Context context = this;
+                mRegisterTask = new AsyncTask<Void, Void, Void>() {
+
+                    @Override
+                    protected Void doInBackground(Void... params) {
+                        // Register on our server
+                        // On server creates a new user
+                        ServerUtilities.register(context, name, email, regId);
+                        return null;
+                    }
+
+                    @Override
+                    protected void onPostExecute(Void result) {
+                        mRegisterTask = null;
+                    }
+
+                };
+                mRegisterTask.execute(null, null, null);
+            }
+        }
+
+        mHandleMessageReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                String newMessage = intent.getExtras().getString(EXTRA_MESSAGE);
+                // Waking up mobile if it is sleeping
+                WakeLocker.acquire(getApplicationContext());
+
+                /**
+                 * Take appropriate action on this message
+                 * depending upon your app requirement
+                 * For now i am just displaying it on the screen
+                 * */
+
+                // Showing received message
+                // lblMessage.append(newMessage + "\n");
+                Toast.makeText(getApplicationContext(), "New Message: " + newMessage, Toast.LENGTH_LONG).show();
+
+                // Releasing wake lock
+                WakeLocker.release();
+            }
+        };
+
+
+        /*
+        protected void onDestroy(){
+            super.onDestroy();
+            if (mRegisterTask != null) {
+                mRegisterTask.cancel(true);
+            }
+            try {
+                unregisterReceiver(mHandleMessageReceiver);
+                GCMRegistrar.onDestroy(this);
+            } catch (Exception e) {
+                Log.e("UnRegister Receiver Error", "> " + e.getMessage());
+            }
+
+        }*/
+
+
         mTitle = mDrawerTitle = getTitle();
 
         // load slide menu items
@@ -66,7 +196,7 @@ public class MyActivity extends Activity {
         // Find People
         navDrawerItems.add(new NavDrawerItem(navMenuTitles[1], navMenuIcons.getResourceId(1, -1)));
         // Photos
-        navDrawerItems.add(new NavDrawerItem(navMenuTitles[2], navMenuIcons.getResourceId(2, -1),true,"4"));
+        navDrawerItems.add(new NavDrawerItem(navMenuTitles[2], navMenuIcons.getResourceId(2, -1), true, "4"));
         // Communities, Will add a counter here
         navDrawerItems.add(new NavDrawerItem(navMenuTitles[3], navMenuIcons.getResourceId(3, -1)));
         // Pages
